0ea293e96796a03580e1693f0ea8bcf5e015b92e
Christabel-dev530 <christabelnwachukwu06@gmail.com>
2026-01-12 15:17:43 +0100

    my second commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 app=Flask(__name__)
 
 @app.route('/')
@@ -13,12 +13,13 @@ def registration():
 def sales():
     return "<h3>welcome to home page</h3>"
 
-@app.route('/multiply')
-def multiply():
-    a=89
-    b=69
-    c=a*b
-    return "this is the multipliction result " + str(c)
+@app.route("/multiply")
+def multiply ():
+    a = 14
+    b = 15
+    c = 20
+    d= a*b*c
+    return render_template("multiply.html", multiply_result=d)
 
 @app.route('/division')
 def division():
@@ -33,6 +34,43 @@ def addition():
     b=69
     c=a+b
     return "this is the addition result " + str(c)
+
+
+@app.route('/sub')
+def sub():
+    a=289
+    b=201
+    c=a-b
+    return "The subtraction result is " + str(c)
+
+
+
+@app.route('/contact')
+def contact():
+    email="uju@yahoo.com"
+    phone="7748848498949"
+    return "this is our contact info or details " + email + "," + phone 
+
+
+@app.route('/homepage')
+def homepage():
+    name="Mrs Christabel"
+    email="peju@yahoo.com"
+    collect="Details are:" +str(email) +str(name)
+    return render_template('homepage.html', collect=collect)
+
+
+@app.route("/maths")
+def maths ():
+    a = 5
+    b = 10
+    c = a+b
+    return render_template("maths.html", maths_result=c)
+
+
+
+
+
 
 
 
